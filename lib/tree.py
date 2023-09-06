3b7e029c8e37f5df7296ddc4987c00ef4d468dc2
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 12:51:23 +0300

    Completed.

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,6 @@
-class Tree:
-  def __init__(self, root = None):
-    self.root = root
+from bs4 import BeautifulSoup
 
-  def get_element_by_id(self, id):
-    pass
+def get_element_by_id(html_content, target_id):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    element = soup.find(id=target_id)
+    return element
